8843a495d6c8863569a230bf2dd6558dd70952ae
Dade Sheng <dade@live.cn>
2017-02-11 13:32:08 -0500

    add AWS solution

--- AWS/app/ec2_examples.py
+++ AWS/app/ec2_examples.py
@@ -4,12 +4,21 @@ from operator import itemgetter
 import boto3
 from flask import render_template, redirect, url_for
 
+from app import config
 from app import webapp
 
 
 @webapp.route('/ec2_examples', methods=['GET'])
 # Display an HTML list of all ec2 instances
 def ec2_list():
+    # create connection to ec2
+    ec2 = boto3.resource('ec2')
+
+    #    instances = ec2.instances.filter(
+    #        Filters=[{'Name': 'instance-state-name', 'Values': ['running']}])
+
+    instances = ec2.instances.all()
+
     return render_template("ec2_examples/list.html", title="EC2 Instances", instances=instances)
 
 
@@ -17,13 +26,22 @@ def ec2_list():
 # Display details about a specific instance.
 def ec2_view(id):
     ec2 = boto3.resource('ec2')
+
     instance = ec2.Instance(id)
+
     client = boto3.client('cloudwatch')
-    metric_name = 'CPUUtilization'  ##    CPUUtilization, NetworkIn, NetworkOut, NetworkPacketsIn,
-    #    NetworkPacketsOut, DiskWriteBytes, DiskReadBytes  .....
+
+    metric_name = 'CPUUtilization'
+
+    ##    CPUUtilization, NetworkIn, NetworkOut, NetworkPacketsIn,
+    #    NetworkPacketsOut, DiskWriteBytes, DiskReadBytes, DiskWriteOps,
+    #    DiskReadOps, CPUCreditBalance, CPUCreditUsage, StatusCheckFailed,
+    #    StatusCheckFailed_Instance, StatusCheckFailed_System
+
 
     namespace = 'AWS/EC2'
     statistic = 'Average'  # could be Sum,Maximum,Minimum,SampleCount,Average
+
     cpu = client.get_metric_statistics(
         Period=1 * 60,
         StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
@@ -33,33 +51,82 @@ def ec2_view(id):
         Statistics=[statistic],
         Dimensions=[{'Name': 'InstanceId', 'Value': id}]
     )
+
     cpu_stats = []
+
     for point in cpu['Datapoints']:
         hour = point['Timestamp'].hour
         minute = point['Timestamp'].minute
-        cpu_stats.append([hour + minute / 60, point['Average']])
+        time = hour + minute / 60
+        cpu_stats.append([time, point['Average']])
+
     cpu_stats = sorted(cpu_stats, key=itemgetter(0))
 
-    network_in = {}
+    statistic = 'Sum'  # could be Sum,Maximum,Minimum,SampleCount,Average
+
+    network_in = client.get_metric_statistics(
+        Period=1 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName='NetworkIn',
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
 
     net_in_stats = []
+
     for point in network_in['Datapoints']:
         hour = point['Timestamp'].hour
         minute = point['Timestamp'].minute
-        net_in_stats.append([hour + minute / 60, point['Sum']])
+        time = hour + minute / 60
+        net_in_stats.append([time, point['Sum']])
+
     net_in_stats = sorted(net_in_stats, key=itemgetter(0))
 
+    network_out = client.get_metric_statistics(
+        Period=5 * 60,
+        StartTime=datetime.utcnow() - timedelta(seconds=60 * 60),
+        EndTime=datetime.utcnow() - timedelta(seconds=0 * 60),
+        MetricName='NetworkOut',
+        Namespace=namespace,  # Unit='Percent',
+        Statistics=[statistic],
+        Dimensions=[{'Name': 'InstanceId', 'Value': id}]
+    )
+
+    net_out_stats = []
+
+    for point in network_out['Datapoints']:
+        hour = point['Timestamp'].hour
+        minute = point['Timestamp'].minute
+        time = hour + minute / 60
+        net_out_stats.append([time, point['Sum']])
+
+        net_out_stats = sorted(net_out_stats, key=itemgetter(0))
+
     return render_template("ec2_examples/view.html", title="Instance Info",
-                           instance=instance, cpu_stats=cpu_stats, net_in_stats=net_in_stats)
+                           instance=instance,
+                           cpu_stats=cpu_stats,
+                           net_in_stats=net_in_stats,
+                           net_out_stats=net_out_stats)
 
 
 @webapp.route('/ec2_examples/create', methods=['POST'])
 # Start a new EC2 instance
 def ec2_create():
+    ec2 = boto3.resource('ec2')
+
+    ec2.create_instances(ImageId=config.ami_id, MinCount=1, MaxCount=1)
+
     return redirect(url_for('ec2_list'))
 
 
 @webapp.route('/ec2_examples/delete/<id>', methods=['POST'])
 # Terminate a EC2 instance
 def ec2_destroy(id):
+    # create connection to ec2
+    ec2 = boto3.resource('ec2')
+
+    ec2.instances.filter(InstanceIds=[id]).terminate()
+
     return redirect(url_for('ec2_list'))
--- AWS/app/s3_examples.py
+++ AWS/app/s3_examples.py
@@ -1,4 +1,5 @@
-from flask import render_template, redirect, url_for
+import boto3
+from flask import render_template, redirect, url_for, request
 
 from app import webapp
 
@@ -6,16 +7,51 @@ from app import webapp
 @webapp.route('/s3_examples', methods=['GET'])
 # Display an HTML list of all s3 buckets.
 def s3_list():
-    return render_template("s3_examples/list.html", title="S3 Buckets", buckets=buckets)
+    # Let's use Amazon S3
+    s3 = boto3.resource('s3')
+
+    # Print out bucket names
+    buckets = s3.buckets.all()
+
+    for b in buckets:
+        name = b.name
+
+    buckets = s3.buckets.all()
+
+    return render_template("s3_examples/list.html", title="s3 Instances", buckets=buckets)
 
 
 @webapp.route('/s3_examples/<id>', methods=['GET'])
 # Display details about a specific bucket.
 def s3_view(id):
+    s3 = boto3.resource('s3')
+
+    bucket = s3.Bucket(id)
+
+    for key in bucket.objects.all():
+        k = key
+
+    keys = bucket.objects.all()
+
     return render_template("s3_examples/view.html", title="S3 Bucket Contents", id=id, keys=keys)
 
 
 @webapp.route('/s3_examples/upload/<id>', methods=['POST'])
 # Upload a new file to an existing bucket
 def s3_upload(id):
+    # check if the post request has the file part
+    if 'new_file' not in request.files:
+        return redirect(url_for('s3_view', id=id))
+
+    new_file = request.files['new_file']
+
+    # if user does not select file, browser also
+    # submit a empty part without filename
+    if new_file.filename == '':
+        return redirect(url_for('s3_view', id=id))
+
+    s3 = boto3.client('s3')
+
+    s3.upload_fileobj(new_file, id, new_file.filename)
+
     return redirect(url_for('s3_view', id=id))
