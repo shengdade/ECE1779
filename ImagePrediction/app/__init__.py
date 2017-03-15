e6aed1524fc229205b2de8905cccb3669a05bbb1
Dade Sheng <dade@live.cn>
2017-03-15 15:56:13 -0400

    delete ImagePrediction

--- ImagePrediction/app/__init__.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask
-
-webapp = Flask(__name__)
-
-# from app import classify
-from app import router
--- ImagePrediction/run.py
+++
@@ -1,5 +0,0 @@
-from app import webapp
-
-# webapp.run('0.0.0.0',5001,debug=True)
-
-webapp.run('0.0.0.0', 80)
